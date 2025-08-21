cc0c7af208c2864836be3eff445f245c45b937b3
appuser <appuser@b41cb2192c6d>
2025-08-21 03:53:46 +0000

    feat: Implementação da tarefa: feat: feat: feat: feat: feat: feat: feat: feat: Criar projeto React com TypeScript e configuração inicial seguindo padrões de mercado (task #github-1) (task #github-pr-45) (task #github-pr-46) (task #github-pr-47) (task #github-pr-48) (task #github-pr-49) (task #github-pr-50) (task #github-pr-51) (task #github-pr-52)

---
+++ eslint.config.js
@@ -0,0 +1,13 @@
+module.exports = {
+  root: true,
+  parser: '@typescript-eslint/parser',
+  plugins: ['@typescript-eslint'],
+  extends: ['plugin:@typescript-eslint/recommended'],
+  parserOptions: {
+    ecmaVersion: 2021,
+    sourceType: 'module',
+  },
+  rules: {
+    // Adicione regras personalizadas se necessário
+  },
+};
